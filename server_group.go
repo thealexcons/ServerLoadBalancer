6e9ca16f453732ed89db710edf4327f897a7af71
Alex <constantingomez@icloud.com>
2020-01-15 22:47:40 +0000

    First implementation of weight round robin.

--- server_group.go
+++ server_group.go
@@ -16,29 +16,64 @@ type ServerGroup struct {
 	retries int // default is 3 retries
 }
 
+// SetRetries sets the number of retries when an error occurs connecting to a node
+func (s *ServerGroup) SetRetries(retries int) {
+	s.retries = retries
+}
+
+// Gets the index of the next node to use
 func (s *ServerGroup) nextIndex() int {
 	return int(atomic.AddUint32(&s.current, uint32(1))) % len(s.nodes)
 }
 
+// Gets all the weights from all nodes
+func (s *ServerGroup) getWeights() []int {
+	weights := []int{}
+	for _, n := range s.nodes {
+		weights = append(weights, n.weight)
+	}
+	return weights
+}
+
 // GetNextPeer returns next active peer to take a connection
-// Uses Weighted Round Robin algorithm
 func (s *ServerGroup) getNextNode() *node {
-	/*
-		// loop entire backends to find out an Alive backend
+	weights := s.getWeights()
+	// If all weights are equal, then use unweighted RR algorithm
+	if allSame(weights) {
 		next := s.nextIndex()
 		l := len(s.nodes) + next // start from next and move a full cycle
 		for i := next; i < l; i++ {
 			idx := i % len(s.nodes) // take an index by modding with length
-			// if we have an alive backend, use it and store if its not the original one
 			if s.nodes[idx].isAlive() {
 				if i != next {
-					atomic.StoreUint32(&s.current, uint32(idx)) // mark the current one
+					atomic.StoreUint32(&s.current, uint32(idx)) // set the current one
 				}
 				return s.nodes[idx]
 			}
 		}
 		return nil
-	*/
+	}
+
+	// Use weighted round robin algorithm if different weights
+	next := s.nextIndex() // next server in queue
+	currentWeight := s.nodes[next].weight
+	gcdOfWeights := calculateGCD(weights)
+	for {
+		idx := next % len(s.nodes)
+		if idx == 0 {
+			currentWeight = currentWeight - gcdOfWeights
+			if currentWeight <= 0 {
+				currentWeight = maxValue(weights)
+				if currentWeight == 0 {
+					return nil
+				}
+			}
+		}
+		if s.nodes[idx].weight >= currentWeight && s.nodes[idx].isAlive() {
+			atomic.StoreUint32(&s.current, uint32(idx))
+			return s.nodes[idx]
+		}
+	}
 }
 
 // markBackendStatus changes a alive status of a node
@@ -52,7 +87,7 @@ func (s *ServerGroup) markBackendStatus(nodeUrl *url.URL, alive bool) {
 }
 
 // AddNode creates a node and adds it to the server group given a nodeUrl and weight
-func (s *ServerGroup) AddNode(nodeUrl string, weight uint) {
+func (s *ServerGroup) AddNode(nodeUrl string, weight int) {
 	addr, err := url.Parse(nodeUrl)
 	if err != nil {
 		log.Fatal("The URL provided '%s' is not a valid address.", nodeUrl)
@@ -67,7 +102,7 @@ func (s *ServerGroup) AddNode(nodeUrl string, weight uint) {
 		if retries < s.retries {
 			select {
 			case <-time.After(10 * time.Millisecond):
-				ctx := context.WithValue(r.Context(), Retry, retries+1)
+				ctx := context.WithValue(r.Context(), retry, retries+1)
 				proxy.ServeHTTP(w, r.WithContext(ctx))
 			}
 			return
@@ -76,9 +111,9 @@ func (s *ServerGroup) AddNode(nodeUrl string, weight uint) {
 		// after n retries, mark this backend as down
 		s.markBackendStatus(addr, false)
 
-		attempts := GetAttemptsFromContext(r)
+		atts := GetAttemptsFromContext(r)
 		log.Printf("%s(%s) Retrying %d\n", r.RemoteAddr, r.URL.Path, attempts)
-		ctx := context.WithValue(r.Context(), Attempts, attempts+1)
+		ctx := context.WithValue(r.Context(), attempts, atts+1)
 		s.LoadBalancer(w, r.WithContext(ctx))
 	}
 
